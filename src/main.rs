708818fc96cf472807f4dcb9477542c66ffb2fc9
RXRD <118821868+RXRDRIRI@users.noreply.github.com>
2022-12-13 20:43:54 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,19 @@
-use std::io ;
-
-fn quantum_integer_count(n : u16 , q : f64) -> f64 {
-    (q.powi(n.try_into().unwrap()) - 1.0)/(q - 1.0) // return this mathematics expression
-}
+use rust_quantum_mathematics::QInteger ; //q integer is a strcuture here that consist of unsigned integer base , q and the q integer which is a floating point number
+mod verify ; // this is a module to read input and make sure that the input is correct
 fn main() {
-
+    let mut casa = QInteger{base : 0 , q : 0.0 , number : 0.0} ; // initialize the initial structure
     println!("Welcome to our Program\n\n\n");
-
     // step 1 : initialize the q variable 
-
-    println!("Please input the q number (q number is a 64 bit floating number) : "); 
-    let mut q = String::new(); 
-    io::stdin().read_line(&mut q)
-        .expect("Failed to Read line :v "); // initialize the q variable that will hold the q value
-    let q: f64 = q.trim().parse()
-        .expect("Please type a number >_<") ; // input is in the form of string. So we have to shadow the q variable and make it into a f64 data
-    println!("The q number is {}" , q);
+    casa.q = verify::verifyq() ; //after that put the q value to the structure 
+    println!("The q variable is {}" , casa.q);
     //step 1 is finished 
-    // step 2 : Initialize the n variable
-    println!("Please input the n number (n number is a 64 bit unisgned integer number) : "); 
-    let mut n = String::new(); 
-    io::stdin().read_line(&mut n)
-        .expect("Failed to Read The Input (T_T) "); // initialize the n variable that will hold the n value
-    let mut quantum_number: f64 = n.trim().parse() // initialize quantum number with the same value as n but in the form of floating number 
-        .expect("Please type a number >_<") ; // input is in the form of string and we have to parse it into f64
-    let n: u16 = n.trim().parse()
-        .expect("Please type a number >_<") ; // After quantum number initialized we shadow the original n and make it into u16 data
-    println!("The n number is {}" , n);
-    //step 2 is finished
+    // step 2 : initialize the base number 
+    casa.base = verify::verifybase() ; //after that put the base value to the structure 
+    println!("The base number is {}" , casa.base);
+    // step 3 : calculate the q number 
+    casa.integer_count();
+    println!("The q integer is {}" , casa.number);
 
-    //step 3 count quantum variable 
-    // if q is one then quantum number is the same as n
-    if q != 1.0 {
-        quantum_number = quantum_integer_count(n , q); // since q is not one then we must compute the q integer 
-    }
-    println!("The quantum integer number is {}" , quantum_number); // I want to be a cat
 }
 
 // this is all for now
